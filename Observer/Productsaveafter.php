dbc005316ef8f606dced13a0e478273b32341a4d
eltonfd@gmail.com <eltonfd@gmail.com>
2020-07-29 14:59:21 -0300

    Após salvar produtos usando api, limpa o cache

--- Observer/Productsaveafter.php
+++ Observer/Productsaveafter.php
@@ -1,6 +1,10 @@
 <?php
 namespace Vexpro\CompraPontos\Observer;
 
+use Magento\Framework\App\PageCache\Version;
+use Magento\Framework\App\Cache\TypeListInterface;
+use Magento\Framework\App\Cache\Frontend\Pool;
+
 class Productsaveafter implements \Magento\Framework\Event\ObserverInterface
 {
     protected $_curl;
@@ -8,22 +12,52 @@ class Productsaveafter implements \Magento\Framework\Event\ObserverInterface
     protected $catalogSession;
     protected $scopeConfig;
 
+    protected $cacheTypeList;
+    protected $cacheFrontendPool;
+
     public function __construct(
         \Magento\Framework\HTTP\Client\Curl $curl,
         \Magento\Framework\Message\ManagerInterface $messageManager,
         \Magento\Catalog\Model\Session $catalogSession,
-        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Framework\App\Cache\Manager $cacheManager,
+        TypeListInterface $cacheTypeList, 
+        Pool $cacheFrontendPool
     )
     {
         $this->_curl = $curl;
         $this->_messageManager = $messageManager;
         $this->catalogSession = $catalogSession;
         $this->scopeConfig = $scopeConfig;
+        $this->cacheManager = $cacheManager;
+        $this->cacheTypeList = $cacheTypeList;
+        $this->cacheFrontendPool = $cacheFrontendPool;
+    }
+
+    public function flushCache()
+    {
+    $_types = [
+                'full_page'
+                ];
+    
+        foreach ($_types as $type) {
+            $this->cacheTypeList->cleanType($type);
+        }
+        foreach ($this->cacheFrontendPool as $cacheFrontend) {
+            $cacheFrontend->getBackend()->clean();
+        }
+    }
+
+    private function cleanCache()
+    {
+        $this->cacheManager->flush($this->cacheManager->getAvailableTypes());
+        // or this
+        //$this->cacheManager->clean($this->cacheManager->getAvailableTypes());
     }
 
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        $myEventData = $observer->getData('myEventData');
+        //$myEventData = $observer->getData('myEventData');
         $_product = $observer->getProduct();  // you will get product object
         $_sku=$_product->getSku(); // for sku
         $_name=$_product->getName();
@@ -43,59 +77,59 @@ class Productsaveafter implements \Magento\Framework\Event\ObserverInterface
         // Usa a API GetCurrentPartner para encontrar o cnpj
         $cnpj_partners = [];
 
-        try{
-
-            // $url_base = 'https://cvale-fidelidade-kernel-dev.azurewebsites.net';
-            $url_base = $this->scopeConfig->getValue('acessos/general/kernel_url', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-            $url = $url_base . '/api/Partner/GetCurrentPartner';
-            $this->_curl->addHeader("Authorization", 'bearer '.$token);
-            $this->_curl->get($url);
-            $response = $this->_curl->getBody();
-            $dados = json_decode($response);
-            $cnpj = $dados->cnpj;
-            array_push($cnpj_partners, $cnpj);
-        }
-        catch (\Exception $e) {
-            $this->_messageManager->addError('Não foi possível conectar com germini');
-        }
-        try{
-            $url = $url_base . '/api/Product/CreateUpdateProduct';
-            $params = [
-                "sku" => $_sku,
-                "barcode" => [
-                    $barcode
-                ],
-                "name" => $_name,
-                "price" => $_price,
-                "unit" => $_unidade,
-                "partners" => $cnpj_partners,
-                "active" => true,
-                "generatePoints" => false,
-                "typeOfPoints" => 0,
-                "value" => $value
-            ];
-            
-            $data_json = json_encode($params);
-
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Accept: text/plain'));
-
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-            $response  = curl_exec($ch);
-
-            curl_close($ch);
-
-            $dados = json_decode($response);
-
-            // Ao criar o produto, o germini devolverá o seu id
-            // $_product->setGerminiId($dados->data->id);
-
-        }
-        catch (\Exception $e) {
-            $this->_messageManager->addError('Não foi possível conectar com germini');
+        // $token vai ser null caso esteja criando ou atualizando
+        // usando a API
+        if ($token)
+        {
+            try{
+                $url_base = $this->scopeConfig->getValue('acessos/general/kernel_url', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+                $url = $url_base . '/api/Partner/GetCurrentPartner';
+                $this->_curl->addHeader("Authorization", 'bearer '.$token);
+                $this->_curl->get($url);
+                $response = $this->_curl->getBody();
+                $dados = json_decode($response);
+                $cnpj = $dados->cnpj;
+                array_push($cnpj_partners, $cnpj);
+            }
+            catch (\Exception $e) {
+                $this->_messageManager->addError('Não foi possível conectar com germini');
+            }
+            try{
+                $url = $url_base . '/api/Product/CreateUpdateProduct';
+                $params = [
+                    "sku" => $_sku,
+                    "barcode" => [
+                        $barcode
+                    ],
+                    "name" => $_name,
+                    "price" => $_price,
+                    "unit" => $_unidade,
+                    "partners" => $cnpj_partners,
+                    "active" => true,
+                    "generatePoints" => false,
+                    "typeOfPoints" => 0,
+                    "value" => $value
+                ];
+                
+                $data_json = json_encode($params);
+    
+                $ch = curl_init();
+                curl_setopt($ch, CURLOPT_URL, $url);
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Accept: text/plain'));
+    
+                curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
+                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    
+                $response  = curl_exec($ch);
+    
+                curl_close($ch);
+            }
+            catch (\Exception $e) {
+                $this->_messageManager->addError('Não foi possível conectar com germini');
+            }
+        } else {
+            $this->cleanCache();
+            //$this->flushCache();
         }
     }
 }
